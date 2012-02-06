dbc0ed0cf699e86134760a9c7d960784202a8ab2
Aleksey V Zapparov <ixti@member.fsf.org>
2012-02-07 00:09:00 +0100

    Fix pie chart

--- app.rb
+++ app.rb
@@ -14,16 +14,10 @@ if ENV['OCTOBLAME_FAKE']
         # simulate we have no data
         return nil if rand <= 0.25
 
-        stats = Array.new(rand 1..42).fill do
-          [
-            rand(1..12345),
-            Faker::Name.name,
-            Digest::MD5.hexdigest(Faker::Internet.email)
-          ]
-        end.sort{ |a,b| b.first <=> a.first }
-        
-        # return fake data
-        {:stats => stats, :time => Time.new.to_i}
+        {
+          :stats  => rand <= 0.5 ? fake_stats(1) : fake_stats(rand 1..42),
+          :time   => Time.new.to_i
+        }
       end
 
       protected
@@ -31,6 +25,16 @@ if ENV['OCTOBLAME_FAKE']
       def rand *args
         @prng = Random.new unless @prng
         @prng.rand *args
+      end
+
+      def fake_stats amount
+        Array.new(amount).fill do
+          [
+            rand(1..12345),
+            Faker::Name.name,
+            Digest::MD5.hexdigest(Faker::Internet.email)
+          ]
+        end.sort{ |a,b| b.first <=> a.first }
       end
     end
   end
